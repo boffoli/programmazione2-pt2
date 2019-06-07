9975e8267a6ec0ef3088d31bed6b27d5514c8c7c
Mirco Sipone <mircosipone@gmail.com>
2019-06-07 12:05:30 +0200

    s

--- 10.16 SuperTypeWildcards/src/SuperTypeWildcards.java
+++ 10.16 SuperTypeWildcards/src/SuperTypeWildcards.java
@@ -1,9 +1,17 @@
 import java.util.*;
 
+class Fruit{};
+class Melon extends Fruit{};
+class Apple extends Fruit{};	//<--
+class Jonathan extends Apple{};
+class Orange extends Jonathan{};
+
 public class SuperTypeWildcards {
   static void writeTo(List<? super Apple> apples) {
     apples.add(new Apple());
     apples.add(new Jonathan());
-    // apples.add(new Fruit()); // Error
+    apples.add(new Orange());
+    //apples.add(new Melon());
+    //apples.add(new Fruit()); // Error
   }
 } 
